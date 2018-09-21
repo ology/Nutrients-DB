2cc7ba47e3a9824d522aab8d1ec2001361c7c173
Gene Boggs <gene.boggs@gmail.com>
2018-09-21 13:25:33 -0700

    Always show a nutrient value

--- lib/Nutrients/DB.pm
+++ lib/Nutrients/DB.pm
@@ -122,7 +122,7 @@ post '/compare' => sub {
         my $nuts = {};
 
         for my $k ( keys %$nutrients2 ) {
-            $nuts->{$k} = $nutrients2->{$k}{value};
+            $nuts->{$k} = $nutrients2->{$k}{value} || 0;
         }
 
         for my $k ( keys %$nutrients1 ) {
