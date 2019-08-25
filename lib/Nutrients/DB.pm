14d7215e97011681bcc7fcbc59823a19b4ac424d
Gene Boggs <gene@ology.net>
2019-08-25 02:55:57 -0700

    Add AUTHOR & COPYRIGHT

--- lib/Nutrients/DB.pm
+++ lib/Nutrients/DB.pm
@@ -147,4 +147,15 @@ __END__
 
 L<Dancer2>
 
+=head1 AUTHOR
+ 
+Gene Boggs <gene@cpan.org>
+ 
+=head1 COPYRIGHT AND LICENSE
+ 
+This software is copyright (c) 2019 by Gene Boggs.
+ 
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+ 
 =cut
